afcebf2af3f6f7271dea426b8eec3df997224902
mescoda <mescodasun@gmail.com>
2019-03-21 16:10:51 +0800

    support partialProps

--- src/withFormikItem.js
+++ src/withFormikItem.js
@@ -12,9 +12,10 @@ import getDisplayName from './getDisplayName';
  * Add `error` `value` `touch` `onChange` `onBlur` props depend on `name` prop setted on `Formik.Field`
  *
  * @param {React.Component} Component wrappedComponent
+ * @param {?Function} getPartialProps partial props getter
  * @return {React.Component} result component
  */
-export default Component => {
+export default (Component, getPartialProps = () => {}) => {
 
     const withFormikItemComponent = props => {
         const name = props.field.name;
@@ -32,6 +33,7 @@ export default Component => {
                     onBlur: () => {
                         props.form.setFieldTouched(name);
                     },
+                    ...getPartialProps(props, name),
                     ...props
                 }}
             />
